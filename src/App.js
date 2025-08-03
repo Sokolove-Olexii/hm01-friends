74a13b4224a848ff67568c70d14f448c9b8ea791
Sokolove-Olexii <130235909+Sokolove-Olexii@users.noreply.github.com>
2025-08-03 06:25:32 -0400

    finished

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// Пример использования (App.js)
+import React from "react";
+import FriendList from "./FriendList";
+import friends from "./friends.json";
+
+function App() {
+  return (
+    <div>
+      <FriendList friends={friends} />
+    </div>
+  );
+}
+
+export default App;
